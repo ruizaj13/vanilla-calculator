dec58587ad3859869534e01f1e7f725b9430c6ae
ruizajdev <juan.ruiz@pretzel.io>
2022-06-26 21:34:56 -0700

    addTodo functionality is done

---
+++ app.js
@@ -0,0 +1,43 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+//Functions
+function addTodo(event) {
+    //prevents refreshing when clicked
+    event.preventDefault();
+
+    //Creates TODO Div
+    const todoDiv = document.createElement('div');
+
+    //adds 'todo' className
+    todoDiv.classList.add('todo');
+
+    //Creates li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //Check Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+
+    //Delete Button
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteButton.classList.add('delete-btn');
+    todoDiv.appendChild(deleteButton);
+
+    //Append To List
+    todoList.appendChild(todoDiv);
+
+    //Clear input
+    todoInput.value = '';
+};
